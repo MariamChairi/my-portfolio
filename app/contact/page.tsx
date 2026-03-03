260fc0fbe0a037f5b31b48ce7264ce6a23606888
MariamChairi <146886902+MariamChairi@users.noreply.github.com>
2026-03-03 15:18:21 +0000

    Update LinkedIn link to use the full URL

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -28,7 +28,7 @@ export default function Contact() {
           <div className="flex-1 flex items-center space-x-10 max-w-5xl">
             
             {/* LinkedIn */}
-            <Link href="https://linkedin.com/in/mariam-chairi-8471a3389" className="group/link flex items-center space-x-6 p-10 bg-slate-900/30 rounded-3xl border border-slate-800/50 hover:bg-slate-800/50 flex-1 min-w-0">
+            <Link href="https://www.linkedin.com/in/mariam-chairi-8471a3389" className="group/link flex items-center space-x-6 p-10 bg-slate-900/30 rounded-3xl border border-slate-800/50 hover:bg-slate-800/50 flex-1 min-w-0">
               <div className="w-20 h-20 bg-slate-950/50 rounded-3xl flex items-center justify-center border-2 border-slate-800/60 shadow-xl group-hover/link:border-blue-500/50">
                 <svg className="w-10 h-10 text-slate-400 group-hover/link:text-blue-400 transition-colors" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.225 0z"/>
